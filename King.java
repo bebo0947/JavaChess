3d3b5679f8cf62e39eaa69e8c9bcc9cb3e1fb01d
Abdelrahman Al-Khawas <abdelrahman,khawas@mail.utoronto.ca>
2020-12-18 19:39:46 -0500

    Added get possMoves for King

--- King.java
+++ King.java
@@ -1,8 +1,10 @@
-package Chess;
+package Chess.JavaChess;
 
 import java.util.ArrayList;
 
 public class King extends Piece {
+	
+	public final int[] delta = {-1, 0, 1};
 	
 	public King(String colour) { 
 		super(colour, 0, "King");
@@ -17,10 +19,24 @@ public class King extends Piece {
 		} return temp;
 	}
 
+	
 	@Override
 	public ArrayList<String> getPossMoves(Board board) {
-		// TODO Auto-generated method stub
-		return null;
+		int[] pos = c2L(this.position);
+		this.possLocs = new ArrayList<>();
+		for (int row: delta) {
+			for (int column: delta) {
+				int[] newLoc = {pos[0] + row, pos[1] + column};
+				Piece atLoc = board.board[newLoc[0]][newLoc[1]];
+				if (atLoc.equals(null) || ! atLoc.colour.equals(this.colour)) {
+					// Either there is nothing there or whats there can be eaten
+					possLocs.add(l2C(newLoc));
+				}
+			}
+		}
+		
+		return this.possLocs;
+		
 	}
 	
 	
